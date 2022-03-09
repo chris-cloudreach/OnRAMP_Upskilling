c90a42779af44b9515939f8b8b7ce47bebfc28f3
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-03-09 20:43:37 +0000

    UPDATE - Implement github actions pipeline

--- vpc_subnets/creates3_ddbLock.tf
+++ vpc_subnets/creates3_ddbLock.tf
@@ -14,18 +14,18 @@
 #   }
 # }
 
-resource "aws_dynamodb_table" "terraform_lock_tbl" {
-  name           = "terraform-lock"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
+# resource "aws_dynamodb_table" "terraform_lock_tbl" {
+#   name           = "terraform-lock"
+#   read_capacity  = 1
+#   write_capacity = 1
+#   hash_key       = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-  tags           = {
-    Name = "terraform-lock"
-  }
-}
+#   tags           = {
+#     Name = "terraform-lock"
+#   }
+# }
